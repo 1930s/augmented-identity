07f3600f2ed561eed0307896706c638690300cd0
Carolyn Lee <cnl2@andrew.cmu.edu>
2019-02-09 11:40:09 -0500

    trying to force websites to show up on next page

--- FlaskPortfolioServer.py
+++ FlaskPortfolioServer.py
@@ -24,8 +24,6 @@ def register():
     userName = request.form ['user']
     password = request.form ['pass']
     confirm = request.form ['confirmPass']
-    if request.method == "POST":
-        print (request.form, file = sys.stderr)
     if request.form ['button'] == "Register" and userName != "" and password != "" and password == confirm:
         userDict [userName] = password
         return redirect("/finishedRegistration")
@@ -70,39 +68,49 @@ def enterDataScreen():
 def enterPortfolioData():
     global personalWebsite
     global github
-    global linkedIn
+    #global linkedIn
     global facebook
     global skills
-    personalWebsiteLink = request.form ["PersonalWebsite"]
-    githubLink = request.form ["Github"]
-    linkedInLink = request.form ["LinkedIn"]
-    facebookLink = request.form ["Facebook"]
-    skillsList = request.form ["Skills"]
-    if personalWebsiteLink != "enter URL" and personalWebsiteLink != "":
-        personalWebsite = personalWebsiteLink
-    if githubLink != "enter URL" and githubLink != "":
-        github = githubLink
-    if linkedInLink != "enter URL" and linkedInLink != "":
-        linkedIn = linkedInLink
-    if facebookLink != "enter URL" and facebookLink != "":
-        facebook = facebookLink
-    if skillsList != "enter your skills" and skillsList != "":
-        skills = skillsList
-    print([personalWebsite, github, linkedIn, facebook, skills], file=sys.stderr)
-    return redirect("/viewData")
+    if request.method == "POST":
+        personalWebsiteLink = request.form ["PersonalWebsite"]
+        githubLink = request.form ["Github"]
+        linkedInLink = request.form ["LinkedIn"]
+        facebookLink = request.form ["Facebook"]
+        skillsList = request.form ["Skills"]
+        if personalWebsiteLink != "enter URL" and personalWebsiteLink != "":
+            personalWebsite = personalWebsiteLink
+        if githubLink != "enter URL" and githubLink != "":
+            github = githubLink
+        if linkedInLink != "enter URL" and linkedInLink != "":
+            linkedIn = linkedInLink
+        if facebookLink != "enter URL" and facebookLink != "":
+            facebook = facebookLink
+        if skillsList != "enter your skills" and skillsList != "":
+            skills = skillsList
+        print([personalWebsite, github, linkedIn, facebook, skills], file=sys.stderr)
+        return redirect(url_for("viewData", linkedIn = linkedIn))
+    if request.method == "GET":
+        git = request.args.get('LinkedIn')
+        return redirect (url_for ("viewData", linkedIn = linkedIn))
 
-@app.route("/viewData")
-def viewData():
-    return render_template("ViewPortfolioData.html")
 
-@app.route("/viewData", methods=["POST", "GET"])
-def placeDatainTable ():
-    print (request.get_json())
-    return jsonify ({request.get_json()})
+@app.route("/viewData/<linkedIn>")
+def viewData(linkedIn):
+    return linkedIn
     
+# 
+# @app.route("/viewData", methods=["POST", "GET"])
+# def placeDataInTable ():
+#     ## i dont think we actually need frikin global variables i think it is all stored on the server somehow...
+#     if reques.method == "POST":
+#          return redirect("/enterPortfolioScreen")
+#     if request.method = "GET":
+#         git = request.args.get('Github')
+#         return (redirect (url_for('')
+#     print (request.get_json())
+#     return jsonify ({request.get_json()})
+#     
 
-# def clickEdit():
-#     return redirect("/enterPortfolioScreen")
 
 if __name__ == '__main__':
     app.run(debug=True, host='128.237.168.24')
