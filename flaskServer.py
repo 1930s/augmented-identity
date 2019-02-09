a78094140ee529ca088d04fda5de928d61fb7545
James Koch <jkoch830@gmail.com>
2019-02-08 22:57:02 -0500

    register page, flask server

--- flaskServer.py
+++ flaskServer.py
@@ -1,10 +1,24 @@
-from flask import Flask, request, render_template
+from __future__ import print_function
+from flask import Flask, request, redirect, url_for, render_template, Response, jsonify
+import sys
+
 app = Flask(__name__)
-    
+userDict = {}
+
+#initial render
 @app.route('/')
 def hello_world():
-    return render_template("htmltest.html")
+    return render_template("registerPage.html")
+    
+# when POST method is called from website
+@app.route("/register", methods=["GET", "POST"])
+def getData():
+    data = request.get_json()
+    userDict.update(data)
+    print(userDict, file=sys.stderr)
+    return render_template("")
 
+@app.route("/")
 
 if __name__ == '__main__':
     app.run(debug=True, host='128.237.173.66')
